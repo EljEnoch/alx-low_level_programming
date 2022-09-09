8849ac41fe5868f4eec857aa0039625b64fb8f35
Elijah Egga <eljenochegga@hotmail.com>
2022-09-08 19:24:59 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - printsthe size of various types
+ * return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
