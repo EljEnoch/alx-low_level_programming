4cc98b63ee453c82e80968233372967b467f9123
Elijah Egga <eljenochegga@hotmail.com>
2022-09-09 18:17:21 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,8 +2,6 @@
 /**
  * main - print if the number is postive, zero, or negative
  * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- *
  * Return: 0
  */
 int main(void)
@@ -15,8 +13,12 @@ int main(void)
 		putchar(ch);
 	}
 
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+
 	putchar('\n');
 
 	return (0);
-
 }
